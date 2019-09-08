14c4e2c8cb1e1f19bbb84cc92e98dbea79e23f81
CircuitBreaker88 <sarris88p@gmail.com>
2019-09-08 01:33:37 -0400

    Last PoW Change

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -112,8 +112,8 @@ public:
 
 		nPoolMaxTransactions = 3;
 		strDarksendPoolDummyAddress = "i7FBJNGDmEsU5wx2m3xw85N8kRgCqA8S7L";
-		nLastPOWBlock = 1000000000; //No Last Block **Remove nLastPOWBlock
-		nPOSStartBlock = 250;
+		nLastPOWBlock = 100000000; //No Last Block **Remove nLastPOWBlock
+		nPOSStartBlock = 1;
 	}
 
 
