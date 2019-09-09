16bf522dda27197c4127dafcd861ee306ed12b27
CircuitBreaker88 <sarris88p@gmail.com>
2019-09-08 20:53:09 -0400

    Protocol Update

--- src/version.h
+++ src/version.h
@@ -30,32 +30,32 @@ static const int DATABASE_VERSION = 70510;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60045;
+static const int PROTOCOL_VERSION = 60055;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_1 = 60040;
-static const int MIN_PEER_PROTO_VERSION_2 = 60040;
-static const int MIN_PEER_PROTO_VERSION_3 = 60045;
+static const int MIN_PEER_PROTO_VERSION_1 = 60050;
+static const int MIN_PEER_PROTO_VERSION_2 = 60050;
+static const int MIN_PEER_PROTO_VERSION_3 = 60055;
 
 // minimum peer version accepted by DarkSendPool
-static const int MIN_POOL_PEER_PROTO_VERSION_1 = 60040;
-static const int MIN_POOL_PEER_PROTO_VERSION_2 = 60040;
-static const int MIN_POOL_PEER_PROTO_VERSION_3 = 60045;
+static const int MIN_POOL_PEER_PROTO_VERSION_1 = 60050;
+static const int MIN_POOL_PEER_PROTO_VERSION_2 = 60050;
+static const int MIN_POOL_PEER_PROTO_VERSION_3 = 60055;
 
 // minimum peer version accepted for InstantX
-static const int MIN_INSTANTX_PROTO_VERSION_1 = 60040;
-static const int MIN_INSTANTX_PROTO_VERSION_2 = 60040;
-static const int MIN_INSTANTX_PROTO_VERSION_3 = 60045;
+static const int MIN_INSTANTX_PROTO_VERSION_1 = 60050;
+static const int MIN_INSTANTX_PROTO_VERSION_2 = 60050;
+static const int MIN_INSTANTX_PROTO_VERSION_3 = 60055;
 
 //! minimum peer version that can receive masternode payments
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 60040;
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 60040;
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_3 = 60045;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 60050;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 60050;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_3 = 60055;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
